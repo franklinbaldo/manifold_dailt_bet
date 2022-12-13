3ef17da81268d5433b6bed1702d2f548a0b4a995
Franklin Silveira Baldo <franklinbaldo@gmail.com>
2022-12-13 10:18:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -24,12 +24,14 @@ def place_bet(market_id, outcome, bet_amount, key):
   return response
 import requests
 
+import requests
+
 def get_market_id(market_url, key):
   # Extract the market slug from the URL
   market_slug = market_url.split("/")[-1]
-
+  
   # Set the request URL and headers
-  url = "https://manifold.markets/api/v0/market/by-slug/" + market_slug
+  url = "https://manifold.markets/api/v0/slug/" + market_slug
   headers = {
     "Authorization": "Key " + key
   }
@@ -37,8 +39,8 @@ def get_market_id(market_url, key):
   # Send the request to the API
   response = requests.get(url, headers=headers)
   
-  # Parse the response to get the market ID
+  # Get the market ID from the response
   market_id = response.json()["id"]
-
+  
   # Return the market ID
   return market_id
